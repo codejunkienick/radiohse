942b8f2064af3c7e0f810cb46bbbc2f58a2409de
Nikita Poteryaev <psychedelicespresso@gmail.com>
2015-11-01 11:17:18 +0500

    redo config

--- src/config.js
+++ src/config.js
@@ -3,35 +3,58 @@ require('babel-core/polyfill');
 const environment = {
   development: {
     isProduction: false
+    port: 3000,
+    apiPort: 3030,
+    app: {
+      title: 'РАДИОВЫШКА',
+      description: 'Радиостанция Высшей Школы Экономики',
+      meta: {
+        charSet: 'utf-8',
+        property: {
+          'og:site_name': 'РАДИОВЫШКА',
+          'og:image': 'http://radiohse.com/logo.jpg',
+          'og:locale': 'ru_RU',
+          'og:title': 'РАДИОВЫШКА',
+          'og:description': 'Радиостанция Высшей Школы Экономики',
+          'twitter:card': 'summary',
+          'twitter:site': '@codejunkienick',
+          'twitter:creator': '@codejunkienick',
+          'twitter:title': 'РАДИОВЫШКА',
+          'twitter:description': 'Радиостанция Высшей Школы Экономики',
+          'twitter:image': 'http://radiohse.com/logo.jpg',
+          'twitter:image:width': '200',
+          'twitter:image:height': '200'
+        }
+      }
+    }
   },
   production: {
     isProduction: true
-  }
-}[process.env.NODE_ENV || 'development'];
-
-module.exports = Object.assign({
-  port: 3000,
-  apiPort: 3030,
-  app: {
-    title: 'РАДИОВЫШКА',
-    description: 'Радиостанция Высшей Школы Экономики',
-    meta: {
-      charSet: 'utf-8',
-      property: {
-        'og:site_name': 'РАДИОВЫШКА',
-        'og:image': 'http://radiohse.com/logo.jpg',
-        'og:locale': 'ru_RU',
-        'og:title': 'РАДИОВЫШКА',
-        'og:description': 'Радиостанция Высшей Школы Экономики',
-        'twitter:card': 'summary',
-        'twitter:site': '@codejunkienick',
-        'twitter:creator': '@codejunkienick',
-        'twitter:title': 'РАДИОВЫШКА',
-        'twitter:description': 'Радиостанция Высшей Школы Экономики',
-        'twitter:image': 'http://radiohse.com/logo.jpg',
-        'twitter:image:width': '200',
-        'twitter:image:height': '200'
+    port: 3000,
+    apiPort: 3030,
+    app: {
+      title: 'РАДИОВЫШКА',
+      description: 'Радиостанция Высшей Школы Экономики',
+      meta: {
+        charSet: 'utf-8',
+        property: {
+          'og:site_name': 'РАДИОВЫШКА',
+          'og:image': 'http://radiohse.com/logo.jpg',
+          'og:locale': 'ru_RU',
+          'og:title': 'РАДИОВЫШКА',
+          'og:description': 'Радиостанция Высшей Школы Экономики',
+          'twitter:card': 'summary',
+          'twitter:site': '@codejunkienick',
+          'twitter:creator': '@codejunkienick',
+          'twitter:title': 'РАДИОВЫШКА',
+          'twitter:description': 'Радиостанция Высшей Школы Экономики',
+          'twitter:image': 'http://radiohse.com/logo.jpg',
+          'twitter:image:width': '200',
+          'twitter:image:height': '200'
+        }
       }
     }
   }
-}, environment);
+}[process.env.NODE_ENV || 'development'];
+
+module.exports = environment;
